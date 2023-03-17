744e1abc4b8f061e5836b23c7eababd22cd1be97
rahma <fijriatirahmaturrizqi@gmail.com>
2023-03-17 14:04:28 +0700

    kalkulator challenge

---
+++ CalculatorCh2V1/CalculatorCh2V1/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("CALCULATOR CHALLENGE 2");
+            Console.WriteLine("================================\n");
+            int hasil = 0;
+            Console.Write("Masukkan operasi hitung [berisi angka dan simbol] : ");
+            string input = Console.ReadLine().Replace(" ", "");
+
+            if (input.Contains("="))
+            {
+                char[] simbol = { '+', '-', '*', ':', '/' };
+                int posisi = input.IndexOfAny(simbol);
+                int posisiEquals = input.IndexOf('=');
+                int x = input.Length - (posisi + 1) - 1;
+                int pertama = Convert.ToInt32(input.Substring(0, posisi));
+                int kedua = Convert.ToInt32(input.Substring(posisi + 1, x));
+                char operasi = input[posisi];
+
+                switch (operasi)
+                {
+                    case '+':
+                        hasil = pertama + kedua;
+                        Console.Write($"{hasil}");
+                        break;
+                    case '-':
+                        hasil = pertama - kedua;
+                        Console.Write($"{hasil}");
+                        break;
+                    case '*':
+                        hasil = pertama * kedua;
+                        Console.Write($"{hasil}");
+                        break;
+                    case '/':
+                    case ':':
+                        hasil = pertama / kedua;
+                        Console.Write($"{hasil}");
+                        break;
+                    default:
+                        Console.Write("\nPilihan anda tidak tersedia");
+                        break;
+                }
+
+                while (true)
+                {
+                    string lanjut = Console.ReadLine();
+                    string[] Lanjut = lanjut.Split(simbol);
+                    int tempat = lanjut.IndexOfAny(simbol);
+                    char Operasi = lanjut[tempat];
+
+                    switch (Operasi)
+                    {
+                        case '+':
+                            int num2 = int.Parse(Lanjut[1]);
+                            hasil = hasil + num2;
+                            Console.Write($"{hasil}");
+                            break;
+                        case '-':
+                            num2 = int.Parse(Lanjut[1]);
+                            hasil = hasil - num2;
+                            Console.Write($"{hasil}");
+                            break;
+                        case '*':
+                            num2 = int.Parse(Lanjut[1]);
+                            hasil = hasil * num2;
+                            Console.Write($"{hasil}");
+                            break;
+                        case '/':
+                        case ':':
+                            num2 = int.Parse(Lanjut[1]);
+                            hasil = hasil / num2;
+                            Console.Write($"{hasil}");
+                            break;
+                        default:
+                            Console.Write("\nPilihan anda tidak tersedia");
+                            break;
+                    }
+                }
+            }
+            else
+            {
+                Console.Write("Operasi tidak valid karena tidak ada tanda = ");
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,43 @@
+# kalkulator-challenge
+
+Kalkulator challenge ini adalah pengembangan dari kalkulator versi sebelumnya yang pernah saya buat yaitu https://github.com/rhmau1/kalkulator-string-simbol
+<br>
+Dibawah ini akan saya tunjukkan contoh penggunaan kalkulator challenge 
+<br> <hr>
+## Contoh 
+Pada saat __pertama kali__ menjalankan kalkulator ini user __harus__ menginputkan operasi hitung yang diakhiri dengan tanda `=` <br>
+Dan saat ingin __melanjutkan__ perhitungan user __tidak perlu__ menambahkan tanda `=` pada akhir operasi hitung karena dapat menyebabkan eror
+```
+CALCULATOR CHALLENGE 2
+================================
+
+Masukkan operasi hitung [berisi angka dan simbol] : 9992 + 18 =
+```
+Apabila operasi perhitungan diatas dijalankan maka akan menghasilkan output sebagai berikut <br>
+```
+9992 + 18 = 10010
+```
+Apabila user ingin melanjutkan perhitungan maka cukup langsung menambahkan operasi hitung disamping hasil perhitungan yang awal. Contoh inputnya seperti ini
+```
+CALCULATOR CHALLENGE 2
+================================
+
+Masukkan operasi hitung [berisi angka dan simbol] : 9992 + 18 =
+9992 + 18 = 10010-8888
+```
+Apabila input diatas dienter maka akan menghasilkan output seperti ini
+```
+10010 - 8888 = 1122
+```
+Untuk __keluar__ dari kalkulator ini user langsung menekan tombol silang yang terdapat pada bagian ujung kanan atas yang ada pada konsol.
+
+<hr>
+
+## Daftar simbol yang dapat digunakan untuk kalkulator ini 
+
+| Simbol | Keterangan |
+|--------|------------|
+| + | Penambahan | 
+| - | Pengurangan | 
+| * | Perkalian | 
+| : | Pembagian | 
